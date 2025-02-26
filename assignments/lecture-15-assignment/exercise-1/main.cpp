e28e5b05e4c3636f990ad7eefdfa25ff61360159
adhya2005 <adhya.vagish@gmail.com>
2025-02-26 14:13:12 -0800

    fixed

--- assignments/lecture-15-assignment/exercise-1/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Create a program that reads in an arbitrary number of grades and computes their average
-
-    return 0;
-}
--- lecture-materials/lecture_05/1-hello-world/main.cpp
+++
@@ -1,9 +0,0 @@
-// Your code goes here
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    cout << "Hello World" << endl;
-    return 0;
-}
